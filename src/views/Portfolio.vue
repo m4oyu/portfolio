29a9921f189f8775b60d7b60af5a17ed0cf7f375
Yuki Yamamoto <[yuki.yamamoto0915@gmail.com]>
2020-03-20 15:27:55 +0900

    portfolio site modified

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,29 +1,47 @@
 <template>
-    <div class="container mt-2">
-        <div class="row">
-            <Card class="col-md-6"></Card>
-            <Card class="col-md-6"></Card>
-            <Card class="col-md-6"></Card>
-            <Card class="col-md-6"></Card>
-            <Card class="col-md-6"></Card>
-            <Card class="col-md-6"></Card>
-            <Card class="col-md-6"></Card>
-            <Card class="col-md-6"></Card>
-            <Card class="col-md-6"></Card>
-            <Card class="col-md-6"></Card>
-        </div>
+  <div class="container mt-2">
+    <div class="row my-3">
+
+<div class="col-md-6">
+  <b-card
+    title="Tetoris"
+    img-src="../assets/tetoris.png"
+    img-alt="Tetoris"
+    img-top
+    tag="article"
+    style="max-width: 40rem;"
+    class="mb-2"
+  > 
+    <b-card-text>
+      株式会社エムティーアイ インターンシップ事前課題
+    </b-card-text>
+
+    <b-button class="mx-1" href="#" variant="primary">Go somewhere</b-button>
+    <b-badge class="mx-1">HTML</b-badge>
+    <b-badge class="mx-1">CSS</b-badge>
+    <b-badge class="mx-1">JS</b-badge>
+  </b-card> 
+</div>
+
+<div class="col-md-6">
+  <b-card
+    title="Address List"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Address List Application"
+    img-top
+    tag="article"
+    style="max-width: 40rem;"
+    class="mb-2"
+  > 
+    <b-card-text>
+      たにぐちまことさんのUdemy教材 成果物
+    </b-card-text>
+    <b-button class="mx-1" href="#" variant="primary">Go somewhere</b-button>
+    <b-badge variant="success" class="mx-1">Vue.js</b-badge>
+    <b-badge variant="danger" class="mx-1">firebase</b-badge>
+  </b-card> 
+</div>
+
     </div>
+  </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import Card from '@/components/Card.vue'
-
-export default {
-    name: 'Portfolio',
-    components: {
-        Card
-    }
-}
-</script>
-
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    publicPath: '/vain-app/',
-    outputDir: 'docs'
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/portfolio/'
+  : '/'
   }
